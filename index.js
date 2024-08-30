c0bf84ae04b3b94cbdc0e378ebddd79c696f3b9b
Rupesh <rupesh@Rupeshs-MacBook-Air.local>
2024-08-30 14:09:30 +0530

    calsi

---
+++ index.js
@@ -0,0 +1,56 @@
+// console.log('hello');
+// console.log(document);
+// id Selection
+// let btn= document.getElementById('one')
+
+// let btn1= document.getElementsByClassName('two')
+
+
+// let h1= document.querySelectorAll('h1')
+
+
+// for(let i of h1){
+    // i.style.color='red'
+// }
+// console.log(h1,"heheh");
+// h1.style.color='red'
+
+
+
+// let arr=[1,2,3,4,5]
+
+// function fun1(){
+//     console.log('hello');
+// }
+
+// let btn= document.querySelector('button')
+
+// btn.addEventListener('click',function(){
+//     console.log('hello');
+    
+// })
+
+
+// let h1= document.getElementById('one')
+// h1.style.color='red'
+
+
+let btn= document.querySelector('button')
+let body= document.querySelector('body')
+
+btn.addEventListener('click',function(){
+    body.style.backgroundColor='red'
+
+})
+
+
+// let inp= document.querySelector('input')
+// let h2= document.querySelector('h2')
+
+// inp.addEventListener('input',function(e){
+//     // console.log(e.target.value);
+//     h2.innerText=e.target.value
+
+// })
+
+
